3b2e7d6cd5671e921a1f3bb48293915d53b5ab64
uros94 <uros.ng@gmail.com>
2024-09-18 11:52:54 +0200

    bugfix: adding the url for the admin page

--- book/book/urls.py
+++ book/book/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path(r'', auth_views.LoginView.as_view(), name='login'),
     path(r'^signup/$', views.signup, name='signup'),
     path(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
